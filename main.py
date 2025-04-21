2adbb96fa1a7e07c393c3dd139521b280e2266ea
mnolan000 <matthewrosenolan@gmail.com>
2025-04-21 18:07:45 +0000

    4/21/2025 class time, drew circle and first attempts to create text boxes

--- main.py
+++ main.py
@@ -16,20 +16,46 @@ clock = pygame.time.Clock()
 dt = 0
 speed = 10
 
+#text box
+base_font = pygame.font.Font(None, 32)
+user_input = " "
+input_box = pygame.Rect(300,400,50,30)
+active_color = pygame.Color("blue")
+passive_color = pygame.Color("green")
+color = passive_color
+      
+
 """Game loop"""
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.quit:
-      running = False
+        running = False
 
-  screen.fill("white")
+      
+    screen.fill("white")
 
-  
-  #update screen
-  pygame.display.flip()
-  #fps
-  dt = clock.tick(speed)/1000
+    #draw the circle
+    pygame.draw.circle(screen, "black", (400,300) , 270)
+    pygame.draw.circle(screen, "white", (400,300) , 265)
+
+    #draw the lines
+    pygame.draw.line(screen, "black", (130,300), (665,300), 3)
+    pygame.draw.line(screen, "black", (400,30), (400,565), 3)
+
+    #draw the textbox
+      #this doesnt work
+    pygame.draw.rect(screen, color, input_box, 20)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        color = active_color
+    else: 
+        color = passive_color
+
+    #update screen
+    pygame.display.flip()
+    #fps
+    dt = clock.tick(speed)/1000
+
 
 #quit
 pygame.quit
